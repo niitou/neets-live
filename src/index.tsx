ee008b2d0e4b4e3e03241265d51ee8d6616d7f5a
noel <noelnolife@gmail.com>
2023-10-09 08:23:12 -0500

    react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MainPage from './Component/MainPage';
+import AboutUsPage from './Component/AboutUsPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/about",
+    element: <AboutUsPage />
+  }
+])
+
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
