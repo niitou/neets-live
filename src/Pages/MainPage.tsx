8943c7dc34ba8bc423ae146cbdf181d9b601aab8
noel <noelnolife@gmail.com>
2023-10-10 03:27:04 -0500

    move to page folder

---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainComponent from '../Component/MainComponent'
+
+function MainPage() {
+  return (
+    <div>
+      MainPage
+      <MainComponent />
+    </div>
+
+  )
+}
+
+export default MainPage
