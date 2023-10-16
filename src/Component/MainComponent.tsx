b04d0da4706a13929d7e38949eb227cdb1aeccd4
noel <noelnolife@gmail.com>
2023-10-16 00:37:03 -0500

    add cleanup and divide offline and online cards

--- src/Component/MainComponent.tsx
+++ src/Component/MainComponent.tsx
@@ -25,18 +25,35 @@ function MainComponent() {
 		for (let index = 0; index < config["kick"].length; index++) {
 			const api_link = "https://kick.com/api/v2/channels/" + config["kick"][index];
 			axios.get(api_link)
-				.then(res => setData(data =>[...data, kickParser(res.data)]))
+				.then(res => setData(data => [...data, kickParser(res.data)]))
 				.catch(err => console.error(err))
+		}
+
+		return () => {
+			setData([])
 		}
 	}, [])
 
 	return (
 		<div>
 			MainComponent
+			<div className="online-wrapper">
 			{
-				data.map((val, idx) => <StreamerCard key={idx} is_live={val.is_live} slug={val.slug} username={val.username}
-				profile_pic={val.profile_pic} session_title={val.session_title} view_count={val.view_count} />)
+				data.map((val, idx) =>
+					val.is_live ? 
+					<StreamerCard key={idx} {...val} /> : <></>
+				)
 			}
+			</div>
+
+			<div className="offline-wrapper">
+			{
+				data.map((val, idx) =>
+					val.is_live ? 
+					<></> : <StreamerCard key={idx} {...val} />
+				)
+			}
+			</div>
 		</div>
 	)
 }
