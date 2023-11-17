61493f397904148beadaa1f78e92c72fa90cf23f
noel <noelnolife@gmail.com>
2023-11-16 23:50:03 -0500

    update main page

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import MainComponent from '../Component/MainComponent'
 import '../Asset/Style/style.css'
+import BannerComponent from '../Component/BannerComponent'
 
 function MainPage() {
   return (
     <div className='wrapper'>
-      MainPage
+      <BannerComponent/>
       <MainComponent />
     </div>
 
